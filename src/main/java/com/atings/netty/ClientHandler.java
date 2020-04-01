e4ab9ce3f5783118453f6211bcfeab9c86bba6ad
tingsxh <57860002+tingsxh@users.noreply.github.com>
2020-04-01 19:00:14 +0800

    Update ClientHandler.java

--- src/main/java/com/atings/netty/ClientHandler.java
+++ src/main/java/com/atings/netty/ClientHandler.java
@@ -1,32 +1,65 @@
 package com.atings.netty;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
 
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.charset.CharsetEncoder;
 
 /***
  * @author xiaohui
  * @created 2020/4/1
  */
-public class ClientHandler extends SimpleChannelInboundHandler<ByteBuffer> {
+public class ClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
 
-    public static void main(String[] args) {
 
-    }
-
-    @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuffer byteBuffer) throws Exception {
-
-    }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        super.channelActive(ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
         super.exceptionCaught(ctx, cause);
+    }
+
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b
+                .group(group)
+                .channel(NioSocketChannel.class)
+                .remoteAddress(new InetSocketAddress("localhost", 8086))
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new ClientHandler());
+                    }
+                });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+        }
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
+        System.out.println("------client 收到返回"+byteBuf.toString(CharsetUtil.UTF_8));
     }
 }
